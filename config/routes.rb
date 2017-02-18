a285a84f98a08ac18cea6901ab2d3d16497d15af
Jason Chen <jasonc1@andrew.cmu.edu>
2017-02-18 16:29:46 -0500

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 KcwWorld::Application.routes.draw do
-
+  resources :articles
   resources :categories
   resources :proverbs
   resources :photos
-    
+
   get 'home' => 'home#index', :as => :home
   root :to => 'home#index'
 
